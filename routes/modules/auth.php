6f0e3e2df85d83bee41925512849701bef062e21
aden-fahri <adenathoriq@gmail.com>
2026-06-18 11:48:50 +0700

    feat(routes): batasi akses halaman employees hanya untuk user yang sudah login

--- routes/modules/auth.php
+++ routes/modules/auth.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-// Karyawan / Employee Management Routes
-Route::prefix('employees')->name('employees.')->group(function () {
+// Karyawan / Employee Management Routes (Protected by auth middleware)
+Route::middleware('auth')->prefix('employees')->name('employees.')->group(function () {
     Route::get('/', [EmployeeController::class, 'index'])->name('index');
     // Here we will add more routes for create, store, edit, update, toggle later
 });
