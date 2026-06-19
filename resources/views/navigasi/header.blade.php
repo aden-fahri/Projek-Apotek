7ef06a5bb1365890dea7239bd4289b8539b12ca3
aden-fahri <adenathoriq@gmail.com>
2026-06-19 13:55:45 +0700

    feat: remove search bar from top header

--- resources/views/navigasi/header.blade.php
+++ resources/views/navigasi/header.blade.php
@@ -1,11 +1,6 @@
 <header class="admin-header">
     {{-- Right Items --}}
     <div class="header-right">
-        <div class="header-search" style="width: 200px;">
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <input type="text" id="search-admin" placeholder="Search...">
-        </div>
-
         <div class="avatar-circle" title="{{ auth()->user()->name }}">
             {{ strtoupper(substr(auth()->user()->name, 0, 2)) }}
         </div>
